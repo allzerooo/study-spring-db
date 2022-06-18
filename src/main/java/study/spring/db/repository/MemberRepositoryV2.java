0ee0904e5694fde0f4c6e4880f022d4800dabe73
ddalam <dychoi.dev@gmail.com>
2022-06-18 21:36:58 +0900

    feature : 커넥션을 파라미터로 유지
    - 커넥션을 파라미터로 전달해 같은 커넥션이 사용되도록 유지
    - 트랜잭션을 사용해 이체 중 예외가 발생했을 때 rollback 되도록 변경

---
+++ src/main/java/study/spring/db/repository/MemberRepositoryV2.java
@@ -0,0 +1,189 @@
+package study.spring.db.repository;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.NoSuchElementException;
+import javax.sql.DataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.support.JdbcUtils;
+import study.spring.db.domain.Member;
+
+/**
+ * 애플리케이션에서 커넥션을 유지하는 방법 - 커넥션을 파라미터로 전달해서 같은 커넥션이 사용되도록 유지
+ */
+@Slf4j
+public class MemberRepositoryV2 {
+
+	private final DataSource dataSource;
+
+	public MemberRepositoryV2(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+
+	public Member save(Member member) throws SQLException {
+		String sql = "insert into member(member_id, money) values (?, ?)";
+
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		try {
+			con = getConnection();
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, member.getMemberId());
+			pstmt.setInt(2, member.getMoney());
+			pstmt.executeUpdate();
+			return member;
+		} catch (SQLException e) {
+			log.error("db error", e);
+			throw e;
+		} finally {
+			close(con, pstmt, null);
+		}
+
+	}
+
+	public Member findById(String memberId) throws SQLException {
+		String sql = "select * from member where member_id = ?";
+
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			con = getConnection();
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, memberId);
+
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				Member member = new Member();
+				member.setMemberId(rs.getString("member_id"));
+				member.setMoney(rs.getInt("money"));
+				return member;
+			} else {
+				throw new NoSuchElementException("member not found memberId=" + memberId);
+			}
+
+		} catch (SQLException e) {
+			log.error("db error", e);
+			throw e;
+		} finally {
+			close(con, pstmt, rs);
+		}
+
+	}
+
+	public Member findById(Connection con, String memberId) throws SQLException {
+		String sql = "select * from member where member_id = ?";
+
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, memberId);
+
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				Member member = new Member();
+				member.setMemberId(rs.getString("member_id"));
+				member.setMoney(rs.getInt("money"));
+				return member;
+			} else {
+				throw new NoSuchElementException("member not found memberId=" + memberId);
+			}
+
+		} catch (SQLException e) {
+			log.error("db error", e);
+			throw e;
+		} finally {
+			JdbcUtils.closeResultSet(rs);
+			JdbcUtils.closeStatement(pstmt);
+			// JdbcUtils.closeConnection(con); 여기에서 커넥션을 닫아버리면 같은 커넥션을 계속 유지할 수 없음 -> 커넥션은 여기서 닫지 않는다
+		}
+
+	}
+
+
+	public void update(String memberId, int money) throws SQLException {
+		String sql = "update member set money=? where member_id=?";
+
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		try {
+			con = getConnection();
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, money);
+			pstmt.setString(2, memberId);
+			int resultSize = pstmt.executeUpdate();
+			log.info("resultSize={}", resultSize);
+		} catch (SQLException e) {
+			log.error("db error", e);
+			throw e;
+		} finally {
+			close(con, pstmt, null);
+		}
+
+	}
+
+	public void update(Connection con, String memberId, int money) throws SQLException {
+		String sql = "update member set money=? where member_id=?";
+
+		PreparedStatement pstmt = null;
+
+		try {
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, money);
+			pstmt.setString(2, memberId);
+			int resultSize = pstmt.executeUpdate();
+			log.info("resultSize={}", resultSize);
+		} catch (SQLException e) {
+			log.error("db error", e);
+			throw e;
+		} finally {
+			JdbcUtils.closeStatement(pstmt);
+			// JdbcUtils.closeConnection(con); 여기에서 커넥션을 닫아버리면 같은 커넥션을 계속 유지할 수 없음 -> 커넥션은 여기서 닫지 않는다
+		}
+
+	}
+
+
+	public void delete(String memberId) throws SQLException {
+		String sql = "delete from member where member_id=?";
+
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		try {
+			con = getConnection();
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, memberId);
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			log.error("db error", e);
+			throw e;
+		} finally {
+			close(con, pstmt, null);
+		}
+
+	}
+
+	private void close(Connection con, Statement stmt, ResultSet rs) {
+		JdbcUtils.closeResultSet(rs);
+		JdbcUtils.closeStatement(stmt);
+		JdbcUtils.closeConnection(con);
+	}
+
+
+	private Connection getConnection() throws SQLException {
+		Connection con = dataSource.getConnection();
+		log.info("get connection={}, class={}", con, con.getClass());
+		return con;
+	}
+
+
+}
---
+++ src/main/java/study/spring/db/service/MemberServiceV2.java
@@ -0,0 +1,63 @@
+package study.spring.db.service;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import study.spring.db.domain.Member;
+import study.spring.db.repository.MemberRepositoryV2;
+
+/**
+ * 애플리케이션에서 커넥션을 유지하는 방법 - 커넥션을 파라미터로 전달해서 같은 커넥션이 사용되도록 유지
+ * 풀을 고려한 종료
+ */
+@RequiredArgsConstructor
+@Slf4j
+public class MemberServiceV2 {
+
+	private final DataSource dataSource;
+	private final MemberRepositoryV2 memberRepository;
+
+	public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+		final Connection con = dataSource.getConnection();
+		try {
+			con.setAutoCommit(false); // 트랜잭션 시작
+			// 비즈니스 로직
+			bizLogic(con, fromId, toId, money);
+			con.commit(); // 성공 시 커밋
+		} catch (Exception e) {
+			con.rollback(); // 실패 시 롤백
+			throw new IllegalStateException(e);
+		} finally {
+			release(con);
+		}
+	}
+
+	private void bizLogic(final Connection con, final String fromId, final String toId, final int money) throws SQLException {
+		final Member fromMember = memberRepository.findById(con, fromId);
+		final Member toMember = memberRepository.findById(con, toId);
+
+		memberRepository.update(con, fromId, fromMember.getMoney() - money); // autocommit 상태로 이 update는 반영됨
+		validation(toMember);
+		memberRepository.update(con, toId, toMember.getMoney() + money);
+	}
+
+	private void validation(final Member toMember) {
+		if (toMember.getMemberId().equals("ex")) {
+			throw new IllegalStateException("이체 중 예외 발생");
+		}
+	}
+
+	private void release(final Connection con) {
+		if (con != null) {
+			try {
+				con.setAutoCommit(true); // con.close()는 커넥션을 종료하는게 아니라 반납하는거라서 default 상태인 auto commit 상태로 커넥션을 변경 후 close
+				con.close();
+			} catch (Exception e) {
+				log.info("error", e);
+			}
+		}
+	}
+
+}
